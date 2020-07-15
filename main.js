dc71f2b2d2396c038b2014492421046fd381ea63
explicit_geek <48882821+explicitgeek@users.noreply.github.com>
2020-07-15 23:37:31 +0530

    implement Fetch API

--- main.js
+++ main.js
@@ -6,7 +6,27 @@ const shortenButton = document.querySelector('#shorten');
 const responseField = document.querySelector('#responseField');
 
 const shortenUrl = () => {
+    const urlToShorten = inputField.value
+    const data = JSON.stringify({destination: urlToShorten})
+    
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type' : 'application/json',
+            'apikey': apiKey 
+        },
+        body: data
+    }).then((response)=> {
+        if(response.ok) {
+            return response.json()
+        }
 
+        throw new Error('Request failed!')
+    },(networkError) => {
+        console.log(networkError.message)
+    }).then((jsonResponse) => {
+        renderResponse(jsonResponse)
+    })
 }
 
 const displayShortUrl = (event) => {
